f80a8dd8b1cb1430558c2ba903ec264bb50c9eb2
tier5 <work@tier5.us>
2018-01-25 11:12:57 +0530

    new line command is remove from the string

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "toor";
-$dbname = "chatfuel";
+$dbname = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
