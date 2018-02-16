d5a6fd9289586077f8ea89bc71f18276cb3884dd
tier5 <work@tier5.us>
2018-02-16 21:17:48 +0530

    file modified

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
 include 'conn.php';
 session_start();
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 if(isset($_POST["action"]))
 $action=$_POST["action"];
 elseif (isset($_GET["action"])) {
@@ -26,16 +27,26 @@ switch ($action) {
 		break;
 
 	case 'url_access':
+	
 
 		$data=$_GET;
 		$first_name=$_GET["first_name"];
 		$last_name=$_GET["last_name"];
-		$c_name=$_GET["c_name"];
-	  
-	  $user_detail=[];
+		$office_name=$_GET["office_name"];
 
-	  $user_detail["messages"][0]["text"]="First Name : ".$first_name." Last Name : ".$last_name." Company Name : ".$c_name;
+	 	$service_url = "http://members.lasvegasrealtor.com/search/v1/realtors?first_name='".$first_name."'&last_name='".$last_name."'&office_name='".$office_name;
+
+   	$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $service_url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$result = curl_exec($curl);
+		curl_close($curl);
+
+		$user_detail=json_decode($result);
+	  $user_detail["messages"][0]["text"]="Full Name : ".$user_detail["full_name"]." Company Name : ".$user_detail["office_name"];
+
 		echo json_encode($user_detail);
+
 		break;
 	
 	default:
