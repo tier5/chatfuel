46b31746bdb2648733e6c6cdb20722b42c3963cf
tier5 <work@tier5.us>
2018-03-06 17:27:51 +0530

    test.php is modified

--- test.php
+++ test.php
@@ -7,7 +7,6 @@ $action=$_POST["action"];
 elseif (isset($_GET["action"])) {
 	$action=$_GET["action"];
 }
-
 switch ($action) {
 	case 'bot':
 		$data=$_GET;
@@ -43,7 +42,6 @@ switch ($action) {
 		$messages = array();
 		$attachment_arr = array();
 		$array1=json_decode($result);
-
 		
 		/*if (gettype($array1) === 'object') {
 			$msg = array('text' =>  "No Search Results!");
@@ -56,43 +54,37 @@ switch ($action) {
 			$obj->set_attributes = $variables_obj;
 			print_r(json_encode($obj));
 		} else{
-
 		
 	////	$resp_arr = json_decode($resp);
 		if(count($array1)){
 			$counter=count($array1);*/
 		
 	//	foreach ($array1 as $rkey => $rvalue) {
+		
 		$btn_obj = new stdClass();
 		$btn_obj->type="phone_number";
-		$btn_obj->url= $array1[1]->office_phone_number;
+		$btn_obj->phone_number= $array1[1]->office_phone_number;
 		$btn_obj->title="call";
 		$elements_btn_array[0]=$btn_obj;
-
 		$elem_objects = new stdClass();
 		$elem_objects->title =$array1[1]->full_name;
 		$elem_objects->image_url="http://159.203.81.237/test/GLVAR_transparent-logo.jpg";
 		$elem_objects->subtitle=$array1[1]->office_name;
 		$elem_objects->buttons = $elements_btn_array;
 		array_push($elements,$elem_objects);
-
 		$payload= new stdClass();
 		$payload->template_type="list";
 		$payload->top_element_style="large";
 		$payload->elements=$elements;
-
 		$attachment = new stdClass();
 		$attachment->type="template";
 		$attachment->payload=$payload;
-
 		$list_view= new stdClass();
 		$list_view->messages[] = ['attachment' => $attachment];
-	
+				
 		//	}
-
 		echo json_encode($list_view);
 	//	}
-
 			
 //}
 	break;
