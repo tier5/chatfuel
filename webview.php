e9309038095dcb20763e60fd835cc76028550104
Tier511 <work@tier5.us>
2018-03-14 14:51:37 +0530

    webview v0.3

--- webview.php
+++ webview.php
@@ -3,41 +3,4 @@
 	// print_r($_SERVER);
 	// exit();
 	$displayUrl = "https://webviews.tier5-development.us/team_core/show_webview.php";
-	echo "messages:[
-      {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'generic',
-            image_aspect_ratio: 'square',
-            elements: [{
-              title: `Hello Freshers`,
-              subtitle: 'Choose your preferences',
-              buttons:[
-                {
-                  type: 'web_url',
-                  url: displayUrl,
-                  title: 'Webview (compact)',
-                  messenger_extensions: true,
-                  webview_height_ratio: 'compact' // Small view
-                },
-                {
-                  type: 'web_url',
-                  url: displayUrl,
-                  title: 'Webview (tall)',
-                  messenger_extensions: true,
-                  webview_height_ratio: 'tall' // Medium view
-                },
-                {
-                  type: 'web_url',
-                  url: displayUrl,
-                  title: 'Webview (full)',
-                  messenger_extensions: true,
-                  webview_height_ratio: 'full' // large view
-                }
-              ]
-            }]
-          }
-        }
-      }
-  ]})";
+	echo '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"generic","image_aspect_ratio":"square","elements":[{"title":"Hello World","subtitle":"Choose your preferences","buttons":[{"type":"web_url","url":"https://webviews.tier5-development.us/team_core/show_webview.php","title":"Webview (compact)","messenger_extensions":true,"webview_height_ratio":"compact"},{"type":"web_url","url":"https://webviews.tier5-development.us/team_core/show_webview.php","title":"Webview (tall)","messenger_extensions":true,"webview_height_ratio":"tall"},{"type":"web_url","url":"https://webviews.tier5-development.us/team_core/show_webview.php","title":"Webview (full)","messenger_extensions":true,"webview_height_ratio":"full"}]}]}}}]}';
