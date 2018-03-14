c785fe15cd3d92e6fcbbbab83f1471ee523cf21b
Tier511 <work@tier5.us>
2018-03-14 23:42:29 +0530

    webview v10.01

--- postBackChatfuel.php
+++ postBackChatfuel.php
@@ -1,46 +1,27 @@
 <?php
 $f_name 		= $_POST['f_name'];
 $l_name 		= $_POST['l_name'];
-$phone_numer 	= $_POST['phone_number'];
-$date 			= $_POST['date'];
-$time 			= $_POST['time'];
-$note 			= $_POST['note'];
-$full_name		= $f_name." ".$l_name;
-$form_url 		= "https://api.chatfuel.com/bots/5a8aa514e4b05207d55947d6/users/1998661866829501/send";
+$phone_numer 	= urlencode($_POST['phone_number']);
+$date 			= urlencode($_POST['date']);
+$time 			= urlencode($_POST['time']);
+$note 			= urlencode($_POST['note']);
+$full_name		= urlencode($f_name." ".$l_name);
+$block          = urlencode("RESPONSE WEBVIEW");
+$form_url 		= "https://api.chatfuel.com/bots/5a8aa514e4b05207d55947d6/users/1998661866829501/send?chatfuel_token=qwYLsCSz8hk4ytd6CPKP4C0oalstMnGdpDjF8YFHPHCieKNc0AfrnjVs91fGuH74&chatfuel_block_name=".$block."&name=".$full_name."&phone_number=".$phone_number."&date_appo=".$date."&time_appo=".$time."&note_appo=".$note."";
 
-//echo $form_url;
+$curl = curl_init();
 
-$data = array(
-	"chatfuel_token"		=>	"qwYLsCSz8hk4ytd6CPKP4C0oalstMnGdpDjF8YFHPHCieKNc0AfrnjVs91fGuH74",
-	"chatfuel_block_name" 	=>   urlencode("RESPONSE WEBVIEW"),
-	"name"					=>   urlencode($full_name),
-	"phone_number"			=> 	 urlencode($phone_number),
-	"date_appo"				=>	 urlencode($date),
-	"time_appo"				=>   urlencode($time),
-	"note_appo"				=>   urlencode($note)
-);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $form_url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
 
-foreach($data as $key=>$value) {
-	$fields_string .= $key.'='.$value.'&'; 
-}
-rtrim($fields_string, '&');
-// echo $fields_string;
-// exit();
-// $s = curl_init(); 
-
-// curl_setopt($s,CURLOPT_URL,$form_url); 
-// curl_setopt($s,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-// curl_setopt($s,CURLOPT_RETURNTRANSFER,true);
-// curl_setopt($s, CURLOPT_PORT , 443); 
-// curl_setopt($s, CURLOPT_POSTFIELDS, $data); 
-// $my_resp = curl_exec($s);
-// echo $my_resp;
-// curl_close($s);
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL, $form_url);
-curl_setopt($ch,CURLOPT_POST, count($data));
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-$result = curl_exec($ch);
-curl_close($ch);
-//check the result
-var_dump($result);  
+$response = curl_exec($curl);
