913dc77b8f9c3a498bf86706edd747b099c77b51
Tier511 <work@tier5.us>
2018-03-14 17:42:06 +0530

    webview v0.4

--- show_webview.php
+++ show_webview.php
@@ -4,6 +4,34 @@
 	<title>Webview</title>
 </head>
 <body>
-	<h1>Shekhar Don't response to demo bot</h1>
+	<form id="schedule_call_form">
+		<table border="1">
+			<tr>
+				<td>First Name</td>
+				<td><input type="text" name="f_name" placeholder="First Name"></td>
+			</tr>
+			<tr>
+				<td>Last Name</td>
+				<td><input type="text" name="l_name" placeholder="Last Name"></td>
+			</tr>
+			<tr>
+				<td>Phone Number</td>
+				<td><input type="text" name="phone_number" placeholder="Phone Number"></td>
+			</tr>
+			<tr>
+				<td>Date</td>
+				<td><input type="text" name="date" placeholder="Date"></td>
+			</tr>
+			<tr>
+				<td>Time</td>
+				<td><input type="text" name="time" placeholder="Time"></td>
+			</tr>
+			<tr>
+				<td>Notes</td>
+				<td><textarea name="note" placeholder="Notes" rows="5"></textarea></td>
+			</tr>
+		</table>
+		<p><button type="submit">Schedule</button></p>
+	</form>
 </body>
 </html>
