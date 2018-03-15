595eb01ca0f47e3696a8eceed681884a747a0944
Tier5 LLC <github@tier5.us>
2018-03-15 18:02:55 +0530

    webview v0.1

---
+++ json_webview_test.php
@@ -0,0 +1,3 @@
+<?php
+
+echo '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"generic","image_aspect_ratio":"square","elements":[{"title":"Schedule a call","subtitle":"Book an appointment with Tier5","buttons":[{"type":"web_url","url":"https://webviews.tier5-development.us/Tuhina_webview/index.php","title":"Schedule","messenger_extensions":true,"webview_height_ratio":"compact"}]}]}}}]}';
