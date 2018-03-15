dd5f4e5a0fdc8a2fdf5c8fff85a23f691ecfe741
Tier511 <work@tier5.us>
2018-03-15 14:13:05 +0530

    webview v10.04

--- postBackChatfuel.php
+++ postBackChatfuel.php
@@ -1,13 +1,12 @@
 <?php
 $f_name 		= $_POST['f_name'];
 $l_name 		= $_POST['l_name'];
-$phone_numer 	= $_POST['phone_number'];
 $date 			= urlencode($_POST['date']);
 $time 			= urlencode($_POST['time']);
 $note 			= urlencode($_POST['note']);
 $full_name		= urlencode($f_name." ".$l_name);
 $block          = urlencode("RESPONSE WEBVIEW");
-$form_url 		= "https://api.chatfuel.com/bots/5a8aa514e4b05207d55947d6/users/1998661866829501/send?chatfuel_token=qwYLsCSz8hk4ytd6CPKP4C0oalstMnGdpDjF8YFHPHCieKNc0AfrnjVs91fGuH74&chatfuel_block_name=".$block."&name=".$full_name."&phone_number_meet=".$phone_number."&date_appo=".$date."&time_appo=".$time."&note_appo=".$note."";
+$form_url 		= "https://api.chatfuel.com/bots/5a8aa514e4b05207d55947d6/users/1998661866829501/send?chatfuel_token=qwYLsCSz8hk4ytd6CPKP4C0oalstMnGdpDjF8YFHPHCieKNc0AfrnjVs91fGuH74&chatfuel_block_name=".$block."&name=".$full_name."&date_appo=".$date."&time_appo=".$time."&note_appo=".$note."";
 
 $curl = curl_init();
 
--- show_webview.php
+++ show_webview.php
@@ -25,10 +25,6 @@
 				<td><input type="text" name="l_name" placeholder="Last Name"></td>
 			</tr>
 			<tr>
-				<td>Phone Number</td>
-				<td><input type="text" name="phone_number" placeholder="Phone Number"></td>
-			</tr>
-			<tr>
 				<td>Date</td>
 				<td><input type="text" name="date" placeholder="Date"></td>
 			</tr>
