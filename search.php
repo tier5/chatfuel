1b858971b518005bd42c8354a65f60c635a54d70
tier5 <work@tier5.us>
2018-04-25 21:31:37 +0530

    odd listing search released

--- search.php
+++ search.php
@@ -101,11 +101,13 @@ function doRequest($my_url = null) {
 function processOutput($resp = null) {
 	
 	if (count($resp)) {
+
 		$elements = array();
 		$elements_btn_array = array();
 		$messages = array();
 		$attachment_arr = array();
 		$resp_arr = json_decode($resp);
+		$resp_count = count($resp_arr);
 		$counter  = 0;
 		if (isset($_GET['start'])) {
 			$paginate_start = $_GET['start'];
@@ -175,15 +177,44 @@ function processOutput($resp = null) {
 					}
 					print_r(json_encode($list_view));
 				} else {
-					$msg = array('text' =>  "No More Results!");
-					$parent = array();
-					array_push($parent,$msg);
-					$obj  = new stdClass();
-					$obj->messages = $parent;
-					$variables_obj = new stdClass();
-					$variables_obj->demo  =404;
-					$obj->set_attributes = $variables_obj;
-					print_r(json_encode($obj));
+					//its for the odd number of result return from the galver search API
+					if( $paginate_start < $resp_count && $paginate_end > $resp_count ){
+						$i = $resp_count - 1;
+						$btn_obj	= new stdClass();
+						$btn_obj->type ="phone_number";
+						$btn_obj->phone_number = $resp_arr[$i]->office_phone_number;
+						$btn_obj->title = "Call";
+						$elements_btn_array[0] = $btn_obj;
+						//array_push($elements_btn_array[0], $btn_obj);
+						// creating element object
+						$elem_objects = new stdClass();
+						$elem_objects->title = $resp_arr[$i]->full_name;
+						$elem_objects->image_url = "http://159.203.81.237/test/GLVAR_transparent-logo.jpg";
+						$elem_objects->subtitle = $resp_arr[$i]->office_name;
+						$elem_objects->buttons = $elements_btn_array;
+						array_push($elements, $elem_objects);
+						// payload
+						$payload = new stdClass();
+						$payload->template_type = "list";
+						$payload->top_element_style = "large";
+						$payload->elements = $elements;
+						// configure chart
+						$attachment = new stdClass();
+						$attachment->type = "template";
+						$attachment->payload = $payload;
+						$list_view  = new stdClass();
+						$list_view->messages[] = ['attachment' => $attachment];
+					}else{
+						$msg = array('text' =>  "No More Results!");
+						$parent = array();
+						array_push($parent,$msg);
+						$obj  = new stdClass();
+						$obj->messages = $parent;
+						$variables_obj = new stdClass();
+						$variables_obj->demo  =404;
+						$obj->set_attributes = $variables_obj;
+						print_r(json_encode($obj));
+					}
 				}
 				
 			} else {
