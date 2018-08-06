882259265dcb684d7e0f3f0459e076cf7866578d
Jon Vaughn <jon@thinkvaughn.com>
2018-08-06 15:48:33 +0530

    counter less than2 modified

--- search.php
+++ search.php
@@ -99,6 +99,7 @@ function doRequest($my_url = null) {
     }
 }
 function processOutput($resp = null) {
+    $elements = []; 
     if (count($resp)) {
         $resp_arr = [];
         foreach (json_decode($resp, true) as $record) {
@@ -130,7 +131,44 @@ function processOutput($resp = null) {
                 //count total number of objects
                 $counter = count($resp_arr);
                 if ($counter <= 2) {
-                    if (($paginate_start == 0 || !isset($paginate_start)) && ($paginate_end == 1 || !isset($paginate_end))) {
+                    for ($i = $paginate_start; $i < $counter; $i++) { 
+                        $btn_obj = new stdClass();
+                        $btn_obj->type ="phone_number";
+                        $btn_obj->phone_number = $resp_arr[$i]->office_phone_number;
+                        $btn_obj->title = "Call";
+                        $elements_btn_array[0] = $btn_obj;
+                        // creating element object
+                        $elem_objects = new stdClass();
+                        $elem_objects->title = $resp_arr[$i]->full_name;
+                        $elem_objects->image_url = "https://glvar.tier5-development.us/dg-realty/dgrealty.jpg";
+                        $elem_objects->subtitle = $resp_arr[$i]->office_name;
+                        $elem_objects->buttons = $elements_btn_array;
+                        array_push($elements, $elem_objects);
+                        // payload
+                        $payload = new stdClass();
+                        $payload->template_type = "list";
+                        $payload->top_element_style = "large";
+                        $payload->elements = $elements;
+                        // configure chart
+                        $attachment = new stdClass();
+                        $attachment->type = "template";
+                        $attachment->payload = $payload;
+                        $list_view  = new stdClass();
+                        $list_view->messages[] = ['attachment' => $attachment];
+                    }
+                    $msg = array('text' =>  "No More Results!");
+                    $parent = array();
+                    array_push($parent,$msg);
+                    $obj  = new stdClass();
+                    $obj->messages = $parent;
+                    $variables_obj = new stdClass();
+                    $variables_obj->demo  =404;
+                    $obj->set_attributes = $variables_obj;
+                    $list_view->messages[] =  $obj;
+                    header('Content-Type: application/json');
+                    print_r(json_encode($list_view));
+                    exit;
+                    /*if (($paginate_start == 0 || !isset($paginate_start)) && ($paginate_end == 1 || !isset($paginate_end))) {
                         header('Content-Type: application/json');
                         print_r(json_encode(makeListView($resp_arr, $counter, $paginate_start, $paginate_end)));
                         exit;
@@ -148,7 +186,7 @@ function processOutput($resp = null) {
                         $variables_obj->demo  =404;
                         $obj->set_attributes = $variables_obj;
                         echo(json_encode($obj));
-                    }
+                    }*/
                 } elseif (array_key_exists($paginate_start, $resp_arr) && array_key_exists($paginate_end, $resp_arr)) {
                     header('Content-Type: application/json');
                     echo(json_encode(makeListView($resp_arr, $counter, $paginate_start, $paginate_end)));
@@ -231,6 +269,5 @@ function makeListView($resp_arr, $counter, $paginate_start, $paginate_end, $elem
         $variables_obj->demo  = 404;
         $list_view->set_attributes = $variables_obj;
     }
-
     return $list_view;
 }
